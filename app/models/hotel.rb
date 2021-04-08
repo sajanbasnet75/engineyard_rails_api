ddc01c740504be17c168939f1f3464c9fa0cb78c
Sajan Basnet <sajan@gurzu.com>
2021-04-08 10:14:28 +0545

    added hotels

---
+++ app/models/hotel.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: hotels
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Hotel < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_145725) do
+ActiveRecord::Schema.define(version: 2021_04_08_042336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2021_04_06_145725) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
+  end
+
+  create_table "hotels", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
