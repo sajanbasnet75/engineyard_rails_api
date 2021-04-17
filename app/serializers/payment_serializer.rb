94aca51fc8659d05601185a0c3bb33476599f279
Sajan Basnet <sajan@gurzu.com>
2021-04-17 22:07:11 +0545

    change type to pay_type

--- app/serializers/payment_serializer.rb
+++ app/serializers/payment_serializer.rb
@@ -4,6 +4,6 @@
 class PaymentSerializer
   include JSONAPI::Serializer
   attributes :total_amt, :card_number, :card_exp_date, :invoice_id,
-             :type, :pay_status, :billing_name, :billing_country,
+             :pay_type, :pay_status, :billing_name, :billing_country,
              :billing_state, :billing_street, :billing_city
 end
